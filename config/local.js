524ef7367736f7870f6d86503ebcc86749b95a7a
Miles Hochstein <miles.hochstein@gmail.com>
2015-05-23 16:20:01 -0700

    local recommit

--- config/local.js
+++ config/local.js
@@ -5,12 +5,6 @@ module.exports = {
   		database 				: 'photoalbum',
 		user     				: 'root',
  		password 				: 'rootfortheteam'
-		},
-	awsVariables:{
-		bucket					: 'photoalbums2-cdn',
-		key						:  'AKIAJVDO4MY65OBXA7IQ',
-		secret					:  '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'
-	}
-
+		}
 	}
 }
